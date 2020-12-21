a33950965fcc053e549fcb15e3f4fde293ea8af6
iqro kurniawan <blogo3x@gmail.com>
2020-12-21 13:56:44 +0700

    64 bit support (#4)
    * WIP: Added initial 64-bit supports
    
    * Added float 64-bits number support
    
    * Added modbus RTU support
    
    * Moved words modification methods into separated class
    
    * Added documentations and some refactoring
    
    * Changed example script
    
    * Changed from big endian to little endian
    
    * Update example.js
    
    * Fixed error undefined variable
    
    * wordsToNum doesnt need to pass mode, as it decode number in LE
    
    * Added conditional check for ip address must be in string
    
    * Update example.js
    
    * _intToWords use byteLength intead wordType
    
    * Fixed reordering byte
    
    * Moved _isLittleEndian to modbus-words.class
    
    * float number woudnt be rounded if both digits and decimalDigits are null
    
    * Added readCoils, readDiscreteInputs, writeCoils, and writeCoil
    
    * Update example.js
    
    * changed default value
    
    * Update README.md
    
    * Removed nested promises
    
    * Update example.js
    
    * Initial attempt
    
    * Fixed reordering on wordsToNum when converting 64bits number
    
    * Fixed reordering on numToWords when converting 64bits number
    
    * Changed byteOrder to wordSwapped, because modbus registers in byte size are always Big Endian
    
    * Added _getByteSizeFromString to get byte size from defined number in a string
    
    * Changed connectRTU to connectRTUBuffered because for long message connectRTU wont catch the entire message
    
    * in wordsToNum if type is not found, returns string conversion

---
+++ src/class/modbus-words.class.js
@@ -0,0 +1,408 @@
+'use strict';
+/**
+ * @typedef {("UINT16"|"INT16"|"UINT32"|"INT32"|"FLOAT"|"FLOAT32"|"DOUBLE"|"FLOAT64")} numberType
+ * */
+
+/**
+ * Round floating number to n digits
+ * @private
+ * @param {number} number - number to be rounded
+ * @param {number} [digits=2] - number of decimal digits
+ * @return {number} - Rounded Number
+ * */
+function _round(num, digits = 2){
+	const pow = 10 ** digits;
+	return Math.round((num + Number.EPSILON) * pow) / pow;
+}
+
+let _config = {
+		byteOrder: undefined,
+		decimalDigits: undefined,
+	};
+
+/**
+ * @class
+ * @package
+ * */
+class modbusWords{
+	/**
+	 * @constructor
+	 * @param {number[]} byteOrder - byte order of modbus register
+	 * @param {number} decimalDigits - number of decimals for floating point
+	 */
+	constructor({byteOrder, decimalDigits}){
+		this._initialize({byteOrder, decimalDigits});
+	}
+
+	/**
+	 * initialize configuration
+	 * @private
+	 * @param {number[]} byteOrder - byte order of modbus register
+	 * @param {number} decimalDigits - number of decimals for floating point
+	 */
+	_initialize({byteOrder, decimalDigits}){
+		const self = this;
+
+		_config.byteOrder = byteOrder;
+		_config.decimalDigits = decimalDigits;
+
+		/**
+		 * size of byte order, should return 4
+		 * storing the size, in case there is possibility to expand it in the future
+		 * @private
+		 * */
+		self._byteOrderLength = _config.byteOrder.length,
+
+		self._bigEndianNotSwapped = (function(){
+				let lastOrder = undefined;
+				for(const order of _config.byteOrder){
+					if(lastOrder === undefined){
+						lastOrder = order;
+						continue;
+					}
+
+					if(lastOrder < order){
+						return false;
+					}
+
+					lastOrder = order;
+				}
+
+				return true;
+			})();
+
+		/**
+		 * check if current process is in little Endian machine or not
+		 * @public
+		 * */
+		self.isLittleEndian = (function() {
+				const buffer = new ArrayBuffer(2);
+				new DataView(buffer).setInt16(0, 256, true);  // littleEndian
+
+				return new Int16Array(buffer)[0] === 256; // Int16Array uses the platform's endianness.
+			})();
+	};
+
+	/**
+	 * Convert integer number to array of unsigned integer 16-bits numbers
+	 * @private
+	 * @param {number} num - number to be converted
+	 * @param {number} byteLength - byte length of number
+	 * @return {number[]} - array of uint16 numbers
+	 */
+	_intToWords(num, byteLength) {
+		const self = this;
+
+		switch(byteLength){
+			case 2: {
+				const byteArray = new Uint8Array([
+					 (num & 0x00FF),
+					 (num & 0xFF00) >> 8,
+				]);
+
+				// return as BigEndian if 1st byte comes before 0th byte, otherwise return as Little Endian
+				for(const order of _config.byteOrder){
+					if(order === 1){
+						return [(byteArray[1] << 8 | byteArray[0])];
+					}
+
+					if(order === 0){
+						return [(byteArray[0] << 8 | byteArray[1])];
+					}
+				}
+				break;
+			}
+
+			case 4: {
+				const byteArray = new Uint8Array([
+					 (num & 0x000000FF),
+					 (num & 0x0000FF00) >> 8,
+					 (num & 0x00FF0000) >> 16,
+					 (num & 0xFF000000) >> 24,
+				]);
+
+				return [
+					(byteArray[_config.byteOrder[0]] << 8 | byteArray[_config.byteOrder[1]]),
+					(byteArray[_config.byteOrder[2]] << 8 | byteArray[_config.byteOrder[3]]),
+				];
+				break;
+			}
+		}
+
+		throw false;
+	};
+
+	/**
+	 * Convert floating number to array of unsigned integer 16-bits numbers
+	 * @private
+	 * @param {number} num - number to be converted
+	 * @return {number[]} - array of uint16 numbers
+	 */
+	_floatToWords(num){
+		const self = this;
+
+		const buffer = new ArrayBuffer(4);
+		const floatView = new Float32Array(buffer);
+
+		floatView[0] = num;
+
+		const arr = Array.from(new Uint8Array(buffer));
+
+		return [
+			(arr[_config.byteOrder[0]] << 8 | arr[_config.byteOrder[1]]),
+			(arr[_config.byteOrder[2]] << 8 | arr[_config.byteOrder[3]]),
+		];
+	};
+
+	/**
+	 * Convert double (float 64-bits) number to array of unsigned integer 16-bits numbers
+	 * @private
+	 * @param {number} num - number to be converted
+	 * @return {number[]} - array of uint16 numbers
+	 */
+	_doubleToWords(num){
+		const self = this;
+
+		const buffer = new ArrayBuffer(8);
+		const floatView = new Float64Array(buffer);
+
+		floatView[0] = num;
+
+		const arr = Array.from(new Uint8Array(buffer));
+
+		return self._bigEndianNotSwapped
+			? [
+				(arr[self._byteOrderLength + _config.byteOrder[0]] << 8 | arr[self._byteOrderLength + _config.byteOrder[1]]),
+				(arr[self._byteOrderLength + _config.byteOrder[2]] << 8 | arr[self._byteOrderLength + _config.byteOrder[3]]),
+				(arr[_config.byteOrder[0]] << 8 | arr[_config.byteOrder[1]]),
+				(arr[_config.byteOrder[2]] << 8 | arr[_config.byteOrder[3]]),
+			]
+			: [
+				(arr[_config.byteOrder[0]] << 8 | arr[_config.byteOrder[1]]),
+				(arr[_config.byteOrder[2]] << 8 | arr[_config.byteOrder[3]]),
+				(arr[self._byteOrderLength + _config.byteOrder[0]] << 8 | arr[self._byteOrderLength + _config.byteOrder[1]]),
+				(arr[self._byteOrderLength + _config.byteOrder[2]] << 8 | arr[self._byteOrderLength + _config.byteOrder[3]]),
+			];
+	};
+
+	/**
+	 * Convert number into array of unsigned integer 16-bits numbers
+	 * @param {number} number - number to be converted
+	 * @param {numberType} type - number data type
+	 * @return {number[]} - array of uint16 numbers
+	 */
+	numToWords(number, type){
+		const self = this;
+
+		if(!type || typeof(type) !== 'string'){
+			throw 'numToWords() : type must be string';
+		}
+
+		switch(type.toUpperCase()){
+			case 'UINT16':
+			case 'INT16': {
+				return self._intToWords(number, self.byteLength(type));
+				break;
+			}
+
+			case 'UINT32':
+			case 'INT32': {
+				return self._intToWords(number, self.byteLength(type));
+				break;
+			}
+
+			case 'FLOAT':
+			case 'FLOAT32': {
+				return self._floatToWords(number);
+				break;
+			}
+
+			case 'DOUBLE':
+			case 'FLOAT64': {
+				return self._doubleToWords(number);
+				break;
+			}
+
+			default: {
+				return 0;
+				break;
+			}
+		}
+	};
+
+	/**
+	 * Convert Buffer or array of uint16 into Little Endian Buffer
+	 * @private
+	 * @param {Buffer|Array.<uint16>} words - Buffer or array of uint16 numbers to be converted
+	 * @param {numberType} type - number data type
+	 * @return {Buffer}
+	 */
+	_wordsToBuffer(words){
+		const self = this;
+
+		if(!Array.isArray(words) && !Buffer.isBuffer(words)){
+			throw `Should pass Array instead of ${typeof(words)}!`;
+		}
+
+		const wordLength = words.length;
+		const byteArray = (function(){
+				if(Array.isArray(words)){
+					const _tmpArray = [];
+
+					for(const word of words){
+						_tmpArray.push((word & 0xFF00) >> 8);
+						_tmpArray.push((word & 0x00FF) >> 0);
+					}
+
+					return _tmpArray;
+				}
+
+				return Array.from(words);
+			})();
+
+		const byteLength = byteArray.length;
+		const reorderedArray = [];
+		for(let idx = 0, _counter = 0; _counter < byteLength; ++idx){
+			if(_config.byteOrder[idx] >= byteLength){
+				continue;
+			}
+
+			const pos = _config.byteOrder[idx] != undefined
+				? _config.byteOrder[idx]
+				: self._byteOrderLength + _config.byteOrder[idx - self._byteOrderLength];
+
+			reorderedArray[pos] = byteArray[_counter++];
+		}
+
+		return Buffer.from(
+			byteLength > self._byteOrderLength && self._bigEndianNotSwapped
+				? [  ...reorderedArray.slice(4,8), ...reorderedArray.slice(0,4), ]
+				: reorderedArray
+		);
+	};
+
+	/**
+	 * Convert Words received from modbus to number
+	 * @param {Buffer|Array.<uint16>} words - Buffer or array of uint16 numbers to be converted
+	 * @param {numberType} type - number data type
+	 * @param {?number} [digits] - number of decimal digits for float, will be default to set decimalDigits in config if left undefined
+	 * @return {number} - converted number
+	 */
+	wordsToNum(words, type, digits){
+		const self = this;
+
+		if(!type || typeof(type) !== 'string'){
+			throw 'wordsToNum() : type must be string';
+		}
+
+		const _buffer = self._wordsToBuffer(words, self.byteLength(type));
+
+		switch(type.toUpperCase()){
+			case 'INT16': {
+				return _buffer.readInt16LE();
+				break;
+			}
+
+			case 'UINT16': {
+				return _buffer.readUInt16LE();
+				break;
+			}
+
+			case 'INT32': {
+				return _buffer.readInt32LE();
+				break;
+			}
+
+			case 'UINT32': {
+				return _buffer.readUInt32LE();
+				break;
+			}
+
+			case 'FLOAT':
+			case 'FLOAT32': {
+				return digits == undefined && _config.decimalDigits == undefined
+					? _buffer.readFloatLE()
+					: _round(_buffer.readFloatLE(), digits == undefined ? _config.decimalDigits : digits);
+				break;
+			}
+
+			case 'DOUBLE':
+			case 'FLOAT64': {
+				return digits == undefined && _config.decimalDigits == undefined
+					? _buffer.readDoubleLE()
+					: _round(_buffer.readDoubleLE(), digits == undefined ? _config.decimalDigits : digits);
+				break;
+			}
+
+			default: {
+				return words.toString();
+				break;
+			}
+		}
+	};
+
+	/**
+	 * Size of data type in bytes
+	 * @param {numberType} type - number data type
+	 * @return {number} - data type size in bytes
+	 */
+	byteLength(type){
+		const self = this;
+
+		if(!type || typeof(type) !== 'string'){
+			throw 'byteLength() : type must be string';
+		}
+
+		switch(type.toUpperCase()){
+			case 'INT16':
+			case 'UINT16': {
+				return 2;
+				break;
+			}
+
+			case 'FLOAT':
+			case 'FLOAT32':
+			case 'INT32':
+			case 'UINT32': {
+				return 4;
+				break;
+			}
+
+			case 'DOUBLE':
+			case 'FLOAT64': {
+				return 8;
+				break;
+			}
+
+			default: {
+				return self._getByteSizeFromString(type);
+				break;
+			};
+		}
+	};
+
+
+	/**
+	 * Get byte size from tpye name, i.e. 'FLOAT64' will return 64. If no number is found, will return 0
+	 * @param {type} type - data type name
+	 * @return {number} - data type size in byte
+	 */
+	_getByteSizeFromString(type){
+		const numbers = String(type).match(/(([\d])+$)/g);
+		return numbers
+			? +numbers[0]
+			: 0;
+	};
+
+	/**
+	 * Size of data type in words
+	 * @param {numberType} type - number data type
+	 * @return {number} - data type size in words
+	 */
+	wordsLength(type){
+		const self = this;
+		return self.byteLength(type) / 2;
+	};
+}
+
+module.exports = modbusWords;
--- src/class/modbus.class.js
+++ src/class/modbus.class.js
@@ -1,305 +1,301 @@
-const EventEmitter = require('events');
-const ModbusRTU = require("modbus-serial");
+const ModbusRTU = require('modbus-serial');
+const modbusWords = require('./modbus-words.class.js');
 
-'use strict;'
-class modbus extends EventEmitter {
-	constructor( modbusConfig ) {
-		super();
+'use strict';
+
+/**
+ * @class
+ * @extends modbusWords
+ * */
+class modbus extends modbusWords{
+	/**
+	 * @constructor
+	 * @param {string} ip - IP destination for modbus TCP
+	 * @param {number|string} port - Port number for modbus TCP or path to serial port for modbus RTU
+	 * @param {number} baud - Baud Rate for modbus RTU
+	 * @param {boolean} [wordSwapped=true] - byte order of modbus register
+	 * @param {?number} [decimalDigits] - number of decimals for floating point
+	 * @param {number} [timeout=100] - timeout in ms
+	 * @param {boolean} [debug=false] - Print debug message
+	 */
+	constructor({ip, port, baud, id = 1, wordSwapped = true, decimalDigits, debug = false, parity = 'none', stopBits = 1, timeout = 100}) {
+		const byteOrder = wordSwapped ? [1,0,3,2] : [3,2,1,0];
+		super({byteOrder, decimalDigits});
 		const self = this;
-		self.config = {
-				ip : modbusConfig.ip,
-				port : modbusConfig.port,
-				id : modbusConfig.id,
-				byteOrder : modbusConfig.byteOrder || [2,3,0,1],
-				decimalDigits : modbusConfig.decimalDigits || 3,
-				debug : modbusConfig.debug ? modbusConfig.debug : false,
-				timeout : modbusConfig.timeout || 100,
+
+		self._config = {
+				ip,
+				port,
+				baud,
+				id,
+				byteOrder,
+				decimalDigits,
+				debug,
+				parity,
+				stopBits,
+				timeout,
 			};
 
-		self.client = new ModbusRTU();
+		self._client = new ModbusRTU();
 
-		// changed opening client method from callback to promise
-		self.openClient = function() {
-			return new Promise(function(resolve, reject) {
-				self.client.connectTCP(self.config.ip, {port : self.config.port}, function(err){
-					//set id for first time connection
-					self.client.setID(self.config.id);
+		/**
+		 * Connection type either TCP or RTU
+		 * Automatically set based on IP, port, and baud values
+		 * */
+		self._connectionType = (function(){
+				if(typeof(self._config.ip) === 'string' && String(self._config.ip).match(/\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b/)){
+					self._connectionOpt = {
+						dst: self._config.ip,
+						options: {
+							port: self._config.port == undefined ? 502 : self._config.port,
+						},
+					};
 
-					if(self.client.isOpen){
-						resolve(true);
-						return;
-					}
+					return 'TCP';
+				}
 
-					if(err){
-						if(self.config.debug)
-							console.error("client open Error",err);
+				if(typeof(self._config.port) === 'string' && typeof(self._config.baud) === 'number'){
+					self._connectionOpt = {
+						dst: self._config.port,
+						options: {
+							baudRate: self._config.baud,
+							parity: self._config.parity,
+							stopBits: self._config.stopBits,
+						},
+					};
 
-						self.client.close();
-						return reject(err);
-					}
-
-					return resolve(self.client.isOpen);
-				});
-			});
-		}
-
+					return 'RTU';
+				}
+			})();
 	}
 
-	readInputRegisters(addr,len = 1){
+	/**
+	 * open connection to target modbus server
+	 * @private
+	 * @return {Promise} Will resolve client Object if successful, otherwise will reject error
+	 * */
+	_openClient(){
 		const self = this;
 
-		// return register value
-		return self.openClient()
-			.then(response => {
-				self.client.setTimeout(self.config.timeout);
-				return self.client.readInputRegisters(addr, len)
-					.then(data => {
-						return Buffer.from(data.buffer);
-					})
-					.catch(error => {
-						if(self.config.debug)
-							console.error('client.readInputRegisters error :',addr,error);
+		return new Promise(function(resolve, reject) {
+			switch(self._connectionType){
+				case 'TCP': {
+					self._client.connectTCP(self._connectionOpt.dst, self._connectionOpt.options, function(error){
+						if(error){
+							return reject(error);
+						}
 
-						return new Promise((resolve,reject) => {
-								reject(false);
-							});
-					})
-					.finally(() => {
-						//~ self.client.close();
+						self._client.isDebugEnabled = self._config.debug;
+						self._client.setID(self._config.id);
+						self._client.setTimeout(self._config.timeout);
+
+						return resolve(self._client);
 					});
-			})
-			.catch(error => {
-				if(self.config.debug)
-					console.error('readInputRegisters error :',addr,error);
 
-				self.client.close();
-				return false;
-			})
-			.finally(() => {
-				self.client.close();
-			});
-	};
+					break;
+				}
 
-	readHoldingRegisters(addr,len = 1){
-		const self = this;
+				case 'RTU': {
+					self._client.connectRTUBuffered(self._connectionOpt.dst, self._connectionOpt.options, function(error){
+						if(error){
+							return reject(error);
+						}
 
-		// return register value
-		return self.openClient()
-			.then(response => {
-				self.client.setTimeout(self.config.timeout);
-				return self.client.readHoldingRegisters(addr, len)
-					.then(data => {
-						return Buffer.from(data.buffer);
-					})
-					.catch(error => {
-						if(self.config.debug)
-							console.error('client.readHoldingRegisters error :',addr,error);
+						self._client.isDebugEnabled = self._config.debug;
+						self._client.setID(self._config.id);
+						self._client.setTimeout(self._config.timeout);
 
-						return new Promise((resolve,reject) => {
-								reject(false);
-							});
-					})
-					.finally(() => {
-						//~ self.client.close();
+						return resolve(self._client);
 					});
-			})
-			.catch(error => {
-				if(self.config.debug)
-					console.error('readHoldingRegisters error :',addr,error);
 
-				self.client.close();
-				return false;
-			})
-			.finally(() => {
-				self.client.close();
-			});
-	};
+					break;
+				}
 
-	writeRegister(addr,value){
-		const self = this;
-
-		// return register value
-		return self.openClient()
-			.then(response => {
-				self.client.setTimeout(self.config.timeout);
-				return self.client.writeRegister(addr, value)
-					.then(data => {
-						return data;
-					})
-					.catch(error => {
-						if(self.config.debug)
-							console.error('client.writeRegisters error :',addr,error);
-
-						return new Promise((resolve,reject) => {
-								reject(false);
-							});
-					})
-					.finally(() => {
-						//~ self.client.close();
-					});
-			})
-			.catch(error => {
-				if(self.config.debug)
-					console.error('writeRegisters error :',addr,error);
-
-				self.client.close();
-				return false;
-			})
-			.finally(() => {
-				self.client.close();
-			});
-	};
-
-	writeRegisters(addr,value){
-		const self = this;
-
-		// return register value
-		return self.openClient()
-			.then(response => {
-				self.client.setTimeout(self.config.timeout);
-				return self.client.writeRegisters(addr, value)
-					.then(data => {
-						return data;
-					})
-					.catch(error => {
-						if(self.config.debug)
-							console.error('client.writeRegisters error :',addr,error);
-
-						return new Promise((resolve,reject) => {
-								reject(false);
-							});
-					})
-					.finally(() => {
-						//~ self.client.close();
-					});
-			})
-			.catch(error => {
-				if(self.config.debug)
-					console.error('writeRegisters error :',addr,error);
-
-				self.client.close();
-				return false;
-			})
-			.finally(() => {
-				self.client.close();
-			});
-	};
-
-	intToWords(num, type = 'WORD') {
-		const self = this;
-
-		const arr = new Uint8Array([
-			 (num & 0x000000ff),
-			 (num & 0x0000ff00) >> 8,
-			 (num & 0x00ff0000) >> 16,
-			 (num & 0xff000000) >> 24,
-		]);
-
-		const WORDS =[
-				(arr[self.config.byteOrder[3]] << 8 | arr[self.config.byteOrder[2]]),
-				(arr[self.config.byteOrder[1]] << 8 | arr[self.config.byteOrder[0]]),
-			];
-
-		return type == 'WORD' ?
-			(self.config.byteOrder[0] > self.config.byteOrder[3] ? [WORDS[0]] : [WORDS[1]]) : // if byteOrder-0 > byteOrder-3, assume it Little Endian
-			WORDS;
-	};
-
-	floatToWords(num){
-		const self = this;
-
-		const buffer = new ArrayBuffer(4);
-		const intView = new Int32Array(buffer);
-		const floatView = new Float32Array(buffer);
-
-		floatView[0] = num;
-
-		const arr = [
-				(intView[0] >>  0) & 0xFF,
-				(intView[0] >>  8) & 0xFF,
-				(intView[0] >> 16) & 0xFF,
-				(intView[0] >> 24) & 0xFF,
-			];
-
-		const WORDS =[
-				(arr[self.config.byteOrder[3]] << 8 | arr[self.config.byteOrder[2]]),
-				(arr[self.config.byteOrder[1]] << 8 | arr[self.config.byteOrder[0]]),
-			];
-
-		return WORDS;
-	};
-
-	numToWords(value = 0,type){
-		const self = this;
-
-		if(!type){
-			throw "numToWords() : type must be defined";
-		}
-
-		if(type === "int32"){
-			return self.intToWords(value,"DWORD");
-		}
-		else if(type === "int16"){
-			return self.intToWords(value,"WORD");
-		}
-		else{
-			return self.floatToWords(value); // assume it's float
-		}
-	};
-
-	wordsToNum(__buffer,type, mode = "read", digits = undefined){
-		const self = this;
-
-		if(!type){
-			throw "wordsToNum() : type must be defined";
-		}
-
-		if(mode != "read" && mode != "write"){
-			throw "wordsToNum() : mode must be 'read' or 'write'";
-		}
-
-		if(type === "int32"){
-			if(mode === "read"){
-				__buffer = Buffer.from([
-						__buffer[self.config.byteOrder[0]],
-						__buffer[self.config.byteOrder[1]],
-						__buffer[self.config.byteOrder[2]],
-						__buffer[self.config.byteOrder[3]],
-					]);
+				default:{
+					return reject('Error: Connection Type is not defined.')
+					break;
+				}
 			}
-			return __buffer.readInt32BE();
-		}
-		else if(type === "int16"){
-			return __buffer.readInt16BE();
-		}
-		else{
-			if(mode === "read"){
-				__buffer = Buffer.from([
-						__buffer[self.config.byteOrder[0]],
-						__buffer[self.config.byteOrder[1]],
-						__buffer[self.config.byteOrder[2]],
-						__buffer[self.config.byteOrder[3]],
-					]);
-			}
-
-			return +__buffer.readFloatBE().toFixed(digits == undefined ? self.config.decimalDigits : digits); // assume it's float
-		}
+		});
 	};
 
-	byteLength(type){
-		if(!type){
-			throw "wordsToNum() : type must be defined";
-		}
+	/**
+	 * Read Input Register (FC=4)
+	 * @param {number} addr - address to read from
+	 * @param {number} [len=1] - number of addresses to read
+	 * @return {Promise.<Buffer>|Promise.<Boolean>} - resolve false if reading is failed, otherwise resolve read Buffer
+	 * */
+	readInputRegisters(addr, len = 1){
+		const self = this;
 
-		if(type === "int32"){
-			return 4;
-		}
-		else if(type === "int16"){
-			return 2;
-		}
-		else{
-			return 4; // assume it's float
-		}
+		return self._openClient()
+			.then(_client => _client.readInputRegisters(addr, len))
+			.then(response => response.buffer)
+			.catch(error => {
+				if(self._config.debug){
+					console.error('readInputRegisters error :', addr, error);
+				}
+
+				return false;
+			})
+			.finally(() => {
+				self._client.close();
+			});
+	};
+
+	/**
+	 * Read Holding Register (FC=3)
+	 * @param {number} addr - address to read from
+	 * @param {number} [len=1] - number of addresses to read
+	 * @return {Promise.<Buffer>|Promise.<Boolean>} - resolve false if reading is failed, otherwise resolve read Buffer
+	 * */
+	readHoldingRegisters(addr, len = 1){
+		const self = this;
+
+		return self._openClient()
+			.then(_client => _client.readHoldingRegisters(addr, len))
+			.then(response => response.buffer)
+			.catch(error => {
+				if(self._config.debug){
+					console.error('readHoldingRegisters error :', addr, error);
+				}
+
+				return false;
+			})
+			.finally(() => {
+				self._client.close();
+			});
+	};
+
+	/**
+	 * Read Discrete Inputs (FC=2)
+	 * @param {number} addr - address to read from
+	 * @param {number} [len=1] - number of addresses to read
+	 * @return {Promise.<Boolean[]>|Promise.<Boolean>} - resolve false if reading is failed, otherwise resolve array of discrete inputs status
+	 * */
+	readDiscreteInputs(addr, len = 1){
+		const self = this;
+
+		return self._openClient()
+			.then(_client => _client.readDiscreteInputs(addr, len))
+			.then(response => response.data.slice(0,len))
+			.catch(error => {
+				if(self._config.debug){
+					console.error('readDiscreteInputs error :', addr, error);
+				}
+
+				return false;
+			})
+			.finally(() => {
+				self._client.close();
+			});
+	};
+
+	/**
+	 * Read Coils (FC=1)
+	 * @param {number} addr - address to read from
+	 * @param {number} [len=1] - number of addresses to read
+	 * @return {Promise.<Boolean[]>|Promise.<Boolean>} - resolve false if reading is failed, otherwise resolve array of coils status
+	 * */
+	readCoils(addr, len = 1){
+		const self = this;
+
+		return self._openClient()
+			.then(_client => _client.readCoils(addr, len))
+			.then(response => response.data.slice(0,len))
+			.catch(error => {
+				if(self._config.debug){
+					console.error('readCoils error :', addr, error);
+				}
+
+				return false;
+			})
+			.finally(() => {
+				self._client.close();
+			});
+	};
+
+	/**
+	 * Write single Holding Register (FC=6)
+	 * @param {number} addr - address to write
+	 * @param {number} value - value to write
+	 * @return {Promise.<Object>|Promise.<Boolean>} - resolve false if reading is failed, otherwise resolve address and length of written registers
+	 * */
+	writeRegister(addr, value){
+		const self = this;
+
+		return self._openClient()
+			.then(_client => _client.writeRegister(addr, value))
+			.then(response => response)
+			.catch(error => {
+				if(self._config.debug){
+					console.error('writeRegister error :', addr, error);
+				}
+
+				return false;
+			})
+			.finally(() => {
+				self._client.close();
+			});
+	};
+
+	/**
+	 * Write multiple Holding Registers (FC=16)
+	 * @param {number} addr - address to write
+	 * @param {number[]} values - array of valuess to write
+	 * @return {Promise.<Object>|Promise.<Boolean>} - resolve false if reading is failed, otherwise resolve address and length of written registers
+	 * */
+	writeRegisters(addr, values){
+		const self = this;
+
+		return self._openClient()
+			.then(_client => _client.writeRegisters(addr, values))
+			.then(response => response)
+			.catch(error => {
+				if(self._config.debug){
+					console.error('writeRegisters error :', addr, error);
+				}
+
+				return false;
+			})
+			.finally(() => {
+				self._client.close();
+			});
+	};
+
+	/**
+	 * Write multiple Coils (FC=15)
+	 * @param {number} addr - address to write
+	 * @param {boolean[]} values - array of values to write
+	 * @return {Promise.<Object>|Promise.<boolean>} - resolve false if reading is failed, otherwise resolve address and length of written registers
+	 * */
+	writeCoils(addr, values){
+		const self = this;
+
+		return self._openClient()
+			.then(_client => _client.writeCoils(addr, values.map(value => value ? true : false)))
+			.then(response => response)
+			.catch(error => {
+				if(self._config.debug){
+					console.error('writeCoils error :', addr, error);
+				}
+
+				return false;
+			})
+			.finally(() => {
+				self._client.close();
+			});
+	};
+
+	/**
+	 * Write Single Coil (modified FC=15)
+	 * @param {number} addr - address to write
+	 * @param {boolean} values - array of values to write
+	 * @return {Promise.<Object>|Promise.<boolean>} - resolve false if reading is failed, otherwise resolve address and length of written registers
+	 * */
+	writeCoil(addr, value){
+		const self = this;
+		return self.writeCoils(addr, [value]);
 	};
 }
 
